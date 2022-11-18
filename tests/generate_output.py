a6f1535dd6893f41f1d377ea7d54c94e1aa36f6b
BuriedInCode <6057651+Buried-In-Code@users.noreply.github.com>
2022-11-18 16:23:42 +1300

    Rework validation to be a Github workflow instead of pytest

---
+++ tests/generate_output.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+
+from dex_starr.schemas.comic_info.schema import ComicInfo
+from dex_starr.schemas.metadata.schema import Metadata
+from dex_starr.schemas.metron_info.schema import MetronInfo
+from dex_starr.schemas.utils import to_comic_info, to_metron_info
+
+FILES_FOLDER = Path(__file__).parent / "files"
+
+
+def output_folder() -> Path:
+    temp = Path(__file__).parent / "output"
+    temp.mkdir(parents=True, exist_ok=True)
+    return temp
+
+
+def generate_metadata():
+    metadata_file = FILES_FOLDER / "Metadata.json"
+    metadata = Metadata.from_file(metadata_file)
+    metadata.to_file(output_folder() / "Metadata.json")
+
+
+def generate_metadata_from_comic_info():
+    comic_info_file = FILES_FOLDER / "ComicInfo.xml"
+    metadata = ComicInfo.from_file(comic_info_file).to_metadata()
+    metadata.to_file(output_folder() / "Metadata-ComicInfo.json")
+
+
+def generate_metadata_from_metron_info():
+    metron_info_file = FILES_FOLDER / "MetronInfo.xml"
+    metadata = MetronInfo.from_file(metron_info_file).to_metadata()
+    metadata.to_file(output_folder() / "Metadata-MetronInfo.json")
+
+
+def generate_comic_info():
+    comic_info_file = FILES_FOLDER / "ComicInfo.xml"
+    comic_info = ComicInfo.from_file(comic_info_file)
+    comic_info.to_file(output_folder() / "ComicInfo.xml")
+
+
+def generate_comic_info_from_metadata():
+    metadata_file = FILES_FOLDER / "Metadata.json"
+    comic_info = to_comic_info(Metadata.from_file(metadata_file))
+    comic_info.to_file(output_folder() / "ComicInfo-Metadata.xml")
+
+
+def generate_metron_info():
+    metron_info_file = FILES_FOLDER / "MetronInfo.xml"
+    metron_info = MetronInfo.from_file(metron_info_file)
+    metron_info.to_file(output_folder() / "MetronInfo.xml")
+
+
+def generate_metron_info_from_metadata():
+    metadata_file = FILES_FOLDER / "Metadata.json"
+    metron_info = to_metron_info(
+        Metadata.from_file(metadata_file),
+        ["Marvel", "League of Comic Geeks", "Grand Comics Database", "Metron", "Comicvine"],
+    )
+    metron_info.to_file(output_folder() / "MetronInfo-Metadata.xml")
+
+
+if __name__ == "__main__":
+    generate_metadata()
+    generate_metadata_from_comic_info()
+    generate_metadata_from_metron_info()
+    generate_comic_info()
+    generate_comic_info_from_metadata()
+    generate_metron_info()
+    generate_metron_info_from_metadata()
